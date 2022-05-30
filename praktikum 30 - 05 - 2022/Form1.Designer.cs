7143812de728129a15d2bb1c34f3cfa9a2b93548
mercyvanneca <mvanneca@student.ciputra.ac.id>
2022-05-30 12:29:41 +0700

    Add project files.

---
+++ praktikum 30 - 05 - 2022/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace praktikum_30___05___2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl_TeamName = new System.Windows.Forms.Label();
+            this.Lbl_Manager = new System.Windows.Forms.Label();
+            this.Lbl_Stadium = new System.Windows.Forms.Label();
+            this.Lbl_TopScorer = new System.Windows.Forms.Label();
+            this.Lbl_WorstDiscipline = new System.Windows.Forms.Label();
+            this.Lbl_Output_TeamName = new System.Windows.Forms.Label();
+            this.Lbl_Output_Manager = new System.Windows.Forms.Label();
+            this.Lbl_Output_Stadium = new System.Windows.Forms.Label();
+            this.Lbl_Output_TopScorer = new System.Windows.Forms.Label();
+            this.Lbl_Output_WorstDiscipline = new System.Windows.Forms.Label();
+            this.Btn_First = new System.Windows.Forms.Button();
+            this.Btn_Prev = new System.Windows.Forms.Button();
+            this.Btn_Next = new System.Windows.Forms.Button();
+            this.Btn_Last = new System.Windows.Forms.Button();
+            this.DGV_View = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_View)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lbl_TeamName
+            // 
+            this.Lbl_TeamName.AllowDrop = true;
+            this.Lbl_TeamName.AutoSize = true;
+            this.Lbl_TeamName.Location = new System.Drawing.Point(21, 61);
+            this.Lbl_TeamName.Name = "Lbl_TeamName";
+            this.Lbl_TeamName.Size = new System.Drawing.Size(71, 13);
+            this.Lbl_TeamName.TabIndex = 0;
+            this.Lbl_TeamName.Text = "Team Name :";
+            // 
+            // Lbl_Manager
+            // 
+            this.Lbl_Manager.AutoSize = true;
+            this.Lbl_Manager.Location = new System.Drawing.Point(21, 88);
+            this.Lbl_Manager.Name = "Lbl_Manager";
+            this.Lbl_Manager.Size = new System.Drawing.Size(55, 13);
+            this.Lbl_Manager.TabIndex = 1;
+            this.Lbl_Manager.Text = "Manager :";
+            // 
+            // Lbl_Stadium
+            // 
+            this.Lbl_Stadium.AutoSize = true;
+            this.Lbl_Stadium.Location = new System.Drawing.Point(21, 115);
+            this.Lbl_Stadium.Name = "Lbl_Stadium";
+            this.Lbl_Stadium.Size = new System.Drawing.Size(51, 13);
+            this.Lbl_Stadium.TabIndex = 2;
+            this.Lbl_Stadium.Text = "Stadium :";
+            // 
+            // Lbl_TopScorer
+            // 
+            this.Lbl_TopScorer.AutoSize = true;
+            this.Lbl_TopScorer.Location = new System.Drawing.Point(21, 142);
+            this.Lbl_TopScorer.Name = "Lbl_TopScorer";
+            this.Lbl_TopScorer.Size = new System.Drawing.Size(66, 13);
+            this.Lbl_TopScorer.TabIndex = 3;
+            this.Lbl_TopScorer.Text = "Top Scorer :";
+            // 
+            // Lbl_WorstDiscipline
+            // 
+            this.Lbl_WorstDiscipline.AutoSize = true;
+            this.Lbl_WorstDiscipline.Location = new System.Drawing.Point(21, 168);
+            this.Lbl_WorstDiscipline.Name = "Lbl_WorstDiscipline";
+            this.Lbl_WorstDiscipline.Size = new System.Drawing.Size(89, 13);
+            this.Lbl_WorstDiscipline.TabIndex = 4;
+            this.Lbl_WorstDiscipline.Text = "Worst Discipline :";
+            // 
+            // Lbl_Output_TeamName
+            // 
+            this.Lbl_Output_TeamName.AutoSize = true;
+            this.Lbl_Output_TeamName.Location = new System.Drawing.Point(133, 61);
+            this.Lbl_Output_TeamName.Name = "Lbl_Output_TeamName";
+            this.Lbl_Output_TeamName.Size = new System.Drawing.Size(16, 13);
+            this.Lbl_Output_TeamName.TabIndex = 5;
+            this.Lbl_Output_TeamName.Text = "...";
+            // 
+            // Lbl_Output_Manager
+            // 
+            this.Lbl_Output_Manager.AutoSize = true;
+            this.Lbl_Output_Manager.Location = new System.Drawing.Point(133, 88);
+            this.Lbl_Output_Manager.Name = "Lbl_Output_Manager";
+            this.Lbl_Output_Manager.Size = new System.Drawing.Size(16, 13);
+            this.Lbl_Output_Manager.TabIndex = 6;
+            this.Lbl_Output_Manager.Text = "...";
+            // 
+            // Lbl_Output_Stadium
+            // 
+            this.Lbl_Output_Stadium.AutoSize = true;
+            this.Lbl_Output_Stadium.Location = new System.Drawing.Point(133, 115);
+            this.Lbl_Output_Stadium.Name = "Lbl_Output_Stadium";
+            this.Lbl_Output_Stadium.Size = new System.Drawing.Size(16, 13);
+            this.Lbl_Output_Stadium.TabIndex = 7;
+            this.Lbl_Output_Stadium.Text = "...";
+            // 
+            // Lbl_Output_TopScorer
+            // 
+            this.Lbl_Output_TopScorer.AutoSize = true;
+            this.Lbl_Output_TopScorer.Location = new System.Drawing.Point(133, 142);
+            this.Lbl_Output_TopScorer.Name = "Lbl_Output_TopScorer";
+            this.Lbl_Output_TopScorer.Size = new System.Drawing.Size(16, 13);
+            this.Lbl_Output_TopScorer.TabIndex = 8;
+            this.Lbl_Output_TopScorer.Text = "...";
+            // 
+            // Lbl_Output_WorstDiscipline
+            // 
+            this.Lbl_Output_WorstDiscipline.AutoSize = true;
+            this.Lbl_Output_WorstDiscipline.Location = new System.Drawing.Point(133, 168);
+            this.Lbl_Output_WorstDiscipline.Name = "Lbl_Output_WorstDiscipline";
+            this.Lbl_Output_WorstDiscipline.Size = new System.Drawing.Size(16, 13);
+            this.Lbl_Output_WorstDiscipline.TabIndex = 9;
+            this.Lbl_Output_WorstDiscipline.Text = "...";
+            // 
+            // Btn_First
+            // 
+            this.Btn_First.Location = new System.Drawing.Point(12, 12);
+            this.Btn_First.Name = "Btn_First";
+            this.Btn_First.Size = new System.Drawing.Size(75, 23);
+            this.Btn_First.TabIndex = 11;
+            this.Btn_First.Text = "<<";
+            this.Btn_First.UseVisualStyleBackColor = true;
+            this.Btn_First.Click += new System.EventHandler(this.Btn_First_Click);
+            // 
+            // Btn_Prev
+            // 
+            this.Btn_Prev.Location = new System.Drawing.Point(93, 12);
+            this.Btn_Prev.Name = "Btn_Prev";
+            this.Btn_Prev.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Prev.TabIndex = 12;
+            this.Btn_Prev.Text = "<";
+            this.Btn_Prev.UseVisualStyleBackColor = true;
+            this.Btn_Prev.Click += new System.EventHandler(this.Btn_Prev_Click);
+            // 
+            // Btn_Next
+            // 
+            this.Btn_Next.Location = new System.Drawing.Point(174, 12);
+            this.Btn_Next.Name = "Btn_Next";
+            this.Btn_Next.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Next.TabIndex = 13;
+            this.Btn_Next.Text = ">";
+            this.Btn_Next.UseVisualStyleBackColor = true;
+            this.Btn_Next.Click += new System.EventHandler(this.Btn_Next_Click);
+            // 
+            // Btn_Last
+            // 
+            this.Btn_Last.Location = new System.Drawing.Point(255, 12);
+            this.Btn_Last.Name = "Btn_Last";
+            this.Btn_Last.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Last.TabIndex = 14;
+            this.Btn_Last.Text = ">>";
+            this.Btn_Last.UseVisualStyleBackColor = true;
+            this.Btn_Last.Click += new System.EventHandler(this.Btn_Last_Click);
+            // 
+            // DGV_View
+            // 
+            this.DGV_View.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_View.Location = new System.Drawing.Point(12, 201);
+            this.DGV_View.Name = "DGV_View";
+            this.DGV_View.Size = new System.Drawing.Size(446, 206);
+            this.DGV_View.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 419);
+            this.Controls.Add(this.DGV_View);
+            this.Controls.Add(this.Btn_Last);
+            this.Controls.Add(this.Btn_Next);
+            this.Controls.Add(this.Btn_Prev);
+            this.Controls.Add(this.Btn_First);
+            this.Controls.Add(this.Lbl_Output_WorstDiscipline);
+            this.Controls.Add(this.Lbl_Output_TopScorer);
+            this.Controls.Add(this.Lbl_Output_Stadium);
+            this.Controls.Add(this.Lbl_Output_Manager);
+            this.Controls.Add(this.Lbl_Output_TeamName);
+            this.Controls.Add(this.Lbl_WorstDiscipline);
+            this.Controls.Add(this.Lbl_TopScorer);
+            this.Controls.Add(this.Lbl_Stadium);
+            this.Controls.Add(this.Lbl_Manager);
+            this.Controls.Add(this.Lbl_TeamName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_View)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_TeamName;
+        private System.Windows.Forms.Label Lbl_Manager;
+        private System.Windows.Forms.Label Lbl_Stadium;
+        private System.Windows.Forms.Label Lbl_TopScorer;
+        private System.Windows.Forms.Label Lbl_WorstDiscipline;
+        private System.Windows.Forms.Label Lbl_Output_TeamName;
+        private System.Windows.Forms.Label Lbl_Output_Manager;
+        private System.Windows.Forms.Label Lbl_Output_Stadium;
+        private System.Windows.Forms.Label Lbl_Output_TopScorer;
+        private System.Windows.Forms.Label Lbl_Output_WorstDiscipline;
+        private System.Windows.Forms.Button Btn_First;
+        private System.Windows.Forms.Button Btn_Prev;
+        private System.Windows.Forms.Button Btn_Next;
+        private System.Windows.Forms.Button Btn_Last;
+        private System.Windows.Forms.DataGridView DGV_View;
+    }
+}
+
---
+++ praktikum 30 - 05 - 2022/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace praktikum_30___05___2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        MySqlConnection sqlConnect = new MySqlConnection("server=localhost;uid=root;pwd=;database=premier_league");
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        String sqlQuery;
+        DataTable dtpremiere_league = new DataTable();
+        int PosisiSekarang = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlQuery = "SELECT t.team_id as `ID Tim`, t.team_name as `Nama Tim`, CONCAT(m.manager_name, ' ', '(', n.nation, ')'), CONCAT(home_stadium, ',', ' ', city, ' ', '(', capacity, ')') FROM team t, manager m, player p, nationality n WHERE t.manager_id = m.manager_id AND m.nationality_id = n.nationality_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtpremiere_league);
+            
+            //sqlQuery = "select concat(p.player_name, " ", SUM(if(d.`type` = "GO" or d.type = "GP", 1, 0)), '(', SUM(if(d.`type` = "GP", 1, 0)), ')') as "Top Scorer" from player p, dmatch d where p.player_id = d.player_id group by player_name order by 1";
+            //sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            //sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            //sqlAdapter.Fill(dtpremiere_league);
+
+            IsiDataPemain(0);
+        }
+
+        public void IsiDataPemain(int Posisi)
+        {
+
+            Lbl_Output_TeamName.Text = dtpremiere_league.Rows[Posisi][0].ToString();
+            Lbl_Output_Manager.Text = dtpremiere_league.Rows[Posisi][1].ToString();
+            Lbl_Output_Stadium.Text = dtpremiere_league.Rows[Posisi][2].ToString();
+            Lbl_Output_TopScorer.Text = dtpremiere_league.Rows[Posisi][0].ToString();
+        }
+
+        private void Btn_First_Click(object sender, EventArgs e)
+        {
+            IsiDataPemain(0);
+        }
+
+        private void Btn_Prev_Click(object sender, EventArgs e)
+        {
+            if (PosisiSekarang > 0)
+            {
+                PosisiSekarang--;
+                IsiDataPemain(PosisiSekarang);
+            }
+            else
+            {
+                MessageBox.Show("ERROR");
+            }
+        }
+
+        private void Btn_Next_Click(object sender, EventArgs e)
+        {
+            if (PosisiSekarang < dtpremiere_league.Rows.Count - 1)
+            {
+                PosisiSekarang++;
+                IsiDataPemain(PosisiSekarang);
+            }
+            else
+            {
+                MessageBox.Show("ERROR");
+            }
+        }
+
+        private void Btn_Last_Click(object sender, EventArgs e)
+        {
+            IsiDataPemain(dtpremiere_league.Rows.Count - 1);
+        }
+    }
+
+}
