da32a3f011af876e7af0aa777acf71f1e422d280
mercyvanneca <mvanneca@student.ciputra.ac.id>
2022-05-30 12:35:17 +0700

    update

--- praktikum 30 - 05 - 2022/Form1.cs
+++ praktikum 30 - 05 - 2022/Form1.cs
@@ -27,7 +27,7 @@ namespace praktikum_30___05___2022
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            sqlQuery = "SELECT t.team_id as `ID Tim`, t.team_name as `Nama Tim`, CONCAT(m.manager_name, ' ', '(', n.nation, ')'), CONCAT(home_stadium, ',', ' ', city, ' ', '(', capacity, ')') FROM team t, manager m, player p, nationality n WHERE t.manager_id = m.manager_id AND m.nationality_id = n.nationality_id";
+            sqlQuery = "SELECT t.team_id as `ID Tim`, t.team_name as `Nama Tim`, CONCAT(m.manager_name, ' ', '(', n.nation, ')'), CONCAT(home_stadium, ',', ' ', city, ' ', '(', capacity, ')') FROM team t, manager m, player p, nationality n WHERE t.manager_id = m.manager_id AND m.nationality_id = n.nationality_id ";
             sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
             sqlAdapter = new MySqlDataAdapter(sqlCommand);
             sqlAdapter.Fill(dtpremiere_league);
@@ -44,9 +44,9 @@ namespace praktikum_30___05___2022
         {
 
             Lbl_Output_TeamName.Text = dtpremiere_league.Rows[Posisi][0].ToString();
-            Lbl_Output_Manager.Text = dtpremiere_league.Rows[Posisi][1].ToString();
-            Lbl_Output_Stadium.Text = dtpremiere_league.Rows[Posisi][2].ToString();
-            Lbl_Output_TopScorer.Text = dtpremiere_league.Rows[Posisi][0].ToString();
+            Lbl_Output_Manager.Text = dtpremiere_league.Rows[Posisi][2].ToString();
+            Lbl_Output_Stadium.Text = dtpremiere_league.Rows[Posisi][3].ToString();
+            //Lbl_Output_TopScorer.Text = dtpremiere_league.Rows[Posisi][0].ToString();
         }
 
         private void Btn_First_Click(object sender, EventArgs e)
